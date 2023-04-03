8e0363bef1989d31128e3e4d7eb8f2c087ff8d07
shahnaz242002 <shahnazavazova@gmail.com>
2023-04-03 15:04:59 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100)
+{
+    Console.WriteLine("Нет денег");
+}
+else
+{
+
+    while (a > 1000)
+    {
+        Console.WriteLine("1000");
+        a = a - 1000;
+    }
+    while (a > 500 && a < 1000)
+    {
+        Console.WriteLine("500");
+        a = a - 500;
+    }
+    while (a >= 100 && a < 500)
+    {
+        Console.WriteLine("100");
+        a = a - 100;
+    }
+
+    Console.WriteLine("возможная выдача");
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while(a>0)
+{
+    int ostatok = a % 10;
+    sum = sum + ostatok;
+    a = a / 10;
+}
+Console.WriteLine(sum);
